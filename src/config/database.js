faa6043133eda1ef7b1394bce52a9b10ca389e41
Caio Valverde <77523760+caiovalverde20@users.noreply.github.com>
2023-03-30 12:41:48 -0300

    Repositorio Base

---
+++ src/config/database.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const config = require("@config");
+
+module.exports = async () => {
+  let DB_URL;
+  if (process.env.NODE_ENV === "prod" || process.env.NODE_ENV === "homolog") {
+    DB_URL = config.db.production;
+  } else if (process.env.NODE_ENV === "dev") {
+    DB_URL = config.db.develop;
+  } else {
+    const { MongoMemoryServer } = require("mongodb-memory-server");
+    const mongod = new MongoMemoryServer();
+    const uri = await mongod.getUri();
+    DB_URL = uri;
+  }
+
+  mongoose.connection.on("connected", () => {
+    console.log("Conectado com o banco de dados!");
+  });
+
+  mongoose.connection.on("error", (err) => {
+    console.log("Erro na conexão com o banco de dados: " + err);
+  });
+
+  mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
---
+++ tests/integration/user.test.js
@@ -0,0 +1,32 @@
+const User = require('../../src/models/User')
+const server = require('../../src/server');
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const data = require('../data.json');
+chai.use(chaiHttp);
+
+describe('User Tests',function() {
+
+    before(async function () {
+        await User.deleteMany({});
+      });
+
+    it('Should create user',async function() {
+        var response = await chai.request(server)
+        .post('/user')
+        .send(data.default_User)
+
+        response.body.user.should.have.property('name');
+      response.body.user.should.have.property('email');
+      });
+
+    it('Should already have existing email',async function() {
+        var response = await chai.request(server)
+        .post('/user')
+        .send(data.default_User)
+
+        response.body.message.should.include('Email já cadastrado');
+      });
+
+})
