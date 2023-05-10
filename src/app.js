d8ca219eedece7b53c494a011b566ff059536294
caiovalverde20 <caiovalverde20@gmail.com>
2023-05-10 04:51:19 -0300

    Import Test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const UserRoutes = require('./routes/UserRoutes');
-const ReportRoutes = require('./routes/UserRoutes');
+const ReportRoutes = require('./routes/ReportRoutes');
 const app  = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -11,6 +11,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
-app.use('/', UserRoutes, ReportRoutes);
+app.use('/', UserRoutes, );
 
 module.exports = app;
