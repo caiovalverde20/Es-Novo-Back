ddbaecd5657cf3905cf5aa7d4ca9319ff457d5d2
caiovalverde20 <caiovalverde20@gmail.com>
2023-05-10 03:45:20 -0300

    Get Report By User

--- src/controllers/ReportController.js
+++ src/controllers/ReportController.js
@@ -27,5 +27,30 @@ module.exports = {
     } catch (error) {
       return res.status(422).send(error.message);
     }
-  }
+  },
+
+  async getReportByUser(req, res) {
+    const { userId, dateStart, dateEnd } = req.params;
+
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(400).send({ message: 'Usuário não encontrado' });
+        }
+
+        const reports = await Report.find({
+            user: user._id,
+            date: {
+                $gte: moment(dateStart, 'DD/MM/YYYY').startOf('day').toDate(),
+                $lte: moment(dateEnd, 'DD/MM/YYYY').endOf('day').toDate()
+            }
+        })
+        .sort({ date: 1, startTime: 1 });
+
+        return res.status(200).send({ reports });
+    } catch (error) {
+        return res.status(422).send(error.message);
+    }
+}
 }
--- src/routes/ReportRoutes.js
+++ src/routes/ReportRoutes.js
@@ -3,5 +3,6 @@ const ReportController = require('@controllers/ReportController');
 const authController = require('@middlewares/Auth');
 
 router.post('/report/:userId', authController.authorizeUser, ReportController.createReport);
+router.get('/report/:userId/:dateStart/:dateEnd', authController.authorizeUser, ReportController.getReportByUser);
 
 module.exports = router;
